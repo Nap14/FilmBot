7f720e82d07c5aeb1b1e2cb1d382a66a84a8c7bb
Andrii Nykolenko <nikolandre14@gmail.com>
2023-03-29 16:05:25 +0300

    in get soup change if else to request with using method

--- database/hdrezka_parser/request.py
+++ database/hdrezka_parser/request.py
@@ -2,45 +2,8 @@ import json
 from colorama import Fore, Style
 
 import requests
-from dataclasses import dataclass, asdict
 from fake_useragent import UserAgent
-
 from bs4 import BeautifulSoup
-
-
-# Define a dataclass for storing video details
-@dataclass
-class Video:
-    external_id: int
-    name: str
-    original_name: str
-    poster: str
-    description: str
-    country: str
-    trailer: str
-    release: str
-    rating: float
-    genres: [str]
-    actors: [str]
-    directors: [str]
-    dubbing: [str]
-    time: int
-    age_limit: int
-
-    def __str__(self):
-        return f"{self.name}, ({self.release})"
-
-
-@dataclass
-class Customer:
-    external_id: int
-    name: str
-    original_name: str
-    birth_date: str
-    profession: [str]
-
-    def __str__(self):
-        return f"{self.name} ({self.birth_date})"
 
 
 # Define a function to retrieve the BeautifulSoup object for a given URL
@@ -57,10 +20,7 @@ def get_soup(
     ] = UserAgent().random
 
     # Make a request to the URL and raise an error if the response code is not 200
-    if method == "POST":
-        response = requests.post(url, *args, **kwargs)
-    else:
-        response = requests.get(url, *args, **kwargs)
+    response = requests.request(method, url, *args, **kwargs)
 
     response.raise_for_status()
 
@@ -69,25 +29,25 @@ def get_soup(
     return BeautifulSoup(response.content, parser)
 
 
-def get_request_data(response_type: str = "base") -> dict:
+def get_request_config(request_type: str = "base") -> dict:
     with open("requests_data.json", "rb") as file:
         data = json.load(fp=file)
 
-    return data[response_type]
+    return data[request_type]
 
 
 def get_trailer_url(film_id: int):
 
-    trailer_data = get_request_data("trailer")
+    trailer_data = get_request_config("trailer")
 
     data = {
         "id": film_id,
     }
 
     response = requests.post(data=data, **trailer_data)
+
     try:
         soup = BeautifulSoup(response.json()["code"], "html.parser")
+        return soup.select_one("iframe")["src"].split("?")[0]
     except KeyError:
         return None
-
-    return soup.select_one("iframe")["src"].split("?")[0]
