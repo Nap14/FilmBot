87386eda637b72a0d109a37cf46c01d6b0e23e08
Andrii Nykolenko <nikolandre14@gmail.com>
2023-03-31 17:38:06 +0300

    add wsgi aplication

--- database/init_django_orm.py
+++ database/init_django_orm.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import django
+from django.core.wsgi import get_wsgi_application
 
 sys.dont_write_bytecode = True
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+application = get_wsgi_application()
 django.setup()
