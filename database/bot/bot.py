fe44ffa827887b71f0de34770083c9cc9b5cb5b7
Andrii Nykolenko <nikolandre14@gmail.com>
2023-04-02 14:50:07 +0300

    add get_message function, add initialize template, fix chat_id

--- database/bot/bot.py
+++ database/bot/bot.py
@@ -9,46 +9,60 @@ from db.models import Film, Chat
 
 
 class FilmBot:
+    """
+    A bot that sends a message about a random film every 3 days.
+
+    The message includes the film's name, release year, trailer link, description, and actors.
+
+    The bot can also send the message about the film on demand via the "/film" command.
+    """
+
     def __init__(self, token, chat_id):
         self.bot = telebot.TeleBot(token)
         self.chat_id = chat_id
-
+        with open("templates/main.html", "rt", encoding="utf-8") as file:
+            self.template = file.read()
 
     def start(self):
         @self.bot.message_handler(commands=["start"])
         def start(message):
-            chat_id = Chat.objects.get_or_create(chat_id=message.chat.chat_id)
-            self.bot.send_message(chat_id, message)
+            chat_id = Chat.objects.get_or_create(chat_id=message.chat.id, spam=True)[0].chat_id
+            self.bot.send_message(chat_id, "Hello in the library of the best films")
+            self._sen_film_every_day(chat_id=chat_id)
 
         @self.bot.message_handler(commands=["film"])
-        def film(message):
-            film = Film.objects.filter(rating__gte=7).order_by("?").first()
+        def get_film(message):
+            film = self._get_message()
 
-            self.bot.send_message(message.chat.id, film.name, parse_mode="HTML")
+            self.bot.send_message(message.chat.id, film, parse_mode="HTML")
 
         Thread(target=self._scheduler, args=()).start()
         self.bot.polling(non_stop=True)
 
-    def _sen_film_every_day(self):
+    def _sen_film_every_day(self, chat_id: int = None):
+        if chat_id is None:
+            chat_id = self.chat_id
+
+        message = self._get_message()
+
+        self.bot.send_message(chat_id, message, parse_mode="HTML")
+
+    def _scheduler(self):
+        schedule.every(3).days.at("19:42").do(self._sen_film_every_day)
+        while True:
+            schedule.run_pending()
+
+    def _get_message(self):
         film = Film.objects.filter(rating__gte=7).order_by("?").first()
         actors = [f"#{actor.name.split()[-1]}" for actor in film.actors.all()]
-        with open("templates/main.html", "rt", encoding="utf-8") as file:
-            self.template = file.read()
 
-        message = self.template.format(
+        return self.template.format(
             name=film.name,
             release=film.release.year,
             trailer=film.trailer,
             description=film.description,
             actors=", ".join(actors),
         )
-
-        self.bot.send_message(self.chat_id, message, parse_mode="HTML")
-
-    def _scheduler(self):
-        schedule.every(3).days.at("19:42").do(self._sen_film_every_day)
-        while True:
-            schedule.run_pending()
 
 
 if __name__ == "__main__":
