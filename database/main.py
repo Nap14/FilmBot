77358a024c4a298b786deefc68caf29be402ab1c
Andrii Nykolenko <nikolandre14@gmail.com>
2023-05-03 19:08:39 +0300

    add param ids to refresh data, change default start value to grater than last film external id

--- database/main.py
+++ database/main.py
@@ -3,20 +3,20 @@ import init_django_orm  # noqa: F401
 from scripts import parse_films, get_empty_ids, get_last_film
 
 
-def refresh_data():
-    parse_films(ids=get_empty_ids(), stop_limit=100)
+def refresh_data(ids: list[int] = get_empty_ids()):
+    parse_films(ids=ids, stop_limit=100)
 
 
 def continue_parsing(
-        start: int = get_last_film(desc=True).external_id, *args, **kwargs
+        start: int = get_last_film(desc=True).external_id + 1, *args, **kwargs
 ):
     parse_films(start=start, *args, **kwargs)
 
 
 def main():
     refresh_data()
-    continue_parsing(count=1000, stop_limit=100)
+    continue_parsing(stop_limit=100)
 
 
 if __name__ == '__main__':
-    refresh_data()
+    refresh_data(get_empty_ids()[-100:])
