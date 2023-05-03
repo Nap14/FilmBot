58108112c9a786f53b46ca447b49629645591b7a
Andrii Nykolenko <nikolandre14@gmail.com>
2023-05-03 18:38:29 +0300

    add continue parsing

--- database/main.py
+++ database/main.py
@@ -1,14 +1,22 @@
 import init_django_orm  # noqa: F401
 
-from scripts import parse_films, get_empty_ids
+from scripts import parse_films, get_empty_ids, get_last_film
 
 
 def refresh_data():
     parse_films(ids=get_empty_ids(), stop_limit=100)
 
 
+def continue_parsing(
+        start: int = get_last_film(desc=True).external_id, *args, **kwargs
+):
+    parse_films(start=start, *args, **kwargs)
+
+
+def main():
+    refresh_data()
+    continue_parsing(count=1000, stop_limit=100)
+
+
 if __name__ == '__main__':
-    pass
-
-
-
+    refresh_data()
